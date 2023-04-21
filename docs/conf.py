3200c050d57e09280da35f5169b6147adbcf52dc
jack <jackmcnish333@gmail.com>
2023-04-21 08:04:31 +0100

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ author = u"Jack Mcnish"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
+    "myst-nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
